928ba25554e4590ae4e6887cc7a2396cd001fb11
Nathaly Andrade <nathalyandradecs@gmail.com>
2020-09-17 11:37:39 -0300

    Adicionando propriedades de conexão com o banco na entidade TipoUsuario

--- FacomFLIX/backend/src/models/TipoUsuario.js
+++ FacomFLIX/backend/src/models/TipoUsuario.js
@@ -1,5 +1,22 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('postgres::memory:');
+const sequelize = new Sequelize('drj55vn7c0l36', 'fcaqeicsrqrwzg', 'bd8eb106388752416a310b8ca07ac2f1b87011a45764b9e3e43b6ea0f53024e1', {
+    host: 'ec2-54-156-121-142.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: '5432',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+    ssl: true,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        },
+        native: true
+        }
+    });
 
  const TipoUsuario = sequelize.define('tipo_usuario', {
     id: {
