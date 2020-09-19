81ec2af865d08fff6d15f850b7aabfa3e4f22bd0
Davidson Ferreira <davidson.tk@hotmail.com>
2020-09-19 01:56:10 -0400

    Configurado jsonwebtoken, criado metodo de autenticação e login de usuário.

--- FacomFLIX/backend/src/controllers/UsuarioController.js
+++ FacomFLIX/backend/src/controllers/UsuarioController.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
 const { models } = require('../../configBD');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
     async mostrarTipoUsuario(req, res) {
@@ -39,7 +42,64 @@ module.exports = {
                 .status(500)
                 .json({ success: false, message: "Erro ao encontrar tipo usuario" });
         }
+    },
+
+    async loginUsuario(req, res) {
+        try {
+            let usuario = await models.Usuario.findOne({ where: { login: req.body.login } });
+            if(usuario){
+                if (await bcrypt.compare(req.body.senha, usuario.senha)) {
+                    let nome = { nome: usuario.nome };
+                    const tokenAcesso = await jwt.sign(nome, process.env.ACCESS_TOKEN_SECRET);
+                    const usuarioLogado = {
+                        id: usuario.id,
+                        nome: usuario.nome,
+                        email: usuario.email,
+                        login: usuario.login,
+                        matricula: usuario.matricula,
+                        tipoUsuario: usuario.tipoUsuario,
+                        token: "Bearer " + tokenAcesso
+                    }
+
+                    return res
+                        .status(200)
+                        .json({ message: "Usuário logado com sucesso" , success: true, usuario: usuarioLogado });
+                } else {
+                    return res
+                        .status(500)
+                        .json({ message: "Senha incorreta" , success: true });
+                }
+            } else {
+                return res
+                    .status(500)
+                    .json({ message: "Usuário ou senha incorreto" , success: true });
+            }
+            
+        } catch (error) {
+            console.log(error.message);
+            return res
+                .status(500)
+                .json({ success: false, message: "Erro ao encontrar tipo usuario" });
+        }
+    },
+
+    async testarLogin(req, res){
+        try {
+            let usuario = await models.Usuario.findOne({ where: { login: req.body.login } });
+            if(usuario){
+                return res
+                    .status(200)
+                    .json({ message: "Token ta onnnnnnnnnnn!!!!!!!!!!!" , success: true, usuario: usuario });
+            }
+            
+        } catch (error) {
+            console.log(error.message);
+            return res
+                .status(500)
+                .json({ success: false, message: "Erro ao encontrar tipo usuario" });
+        }
     }
+
 }
 
    // function validarSenha(senha) {
--- FacomFLIX/backend/src/routes.js
+++ FacomFLIX/backend/src/routes.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const routes = express.Router();
 
 const UsuarioController = require("./controllers/UsuarioController");
 
+const autenticarUsuario = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) {
+        return res
+            .status(401)
+            .json({ success: false, message: "Usuário não está autenticado" }).end();
+    }
 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, usuario) => {
+        if(err) {
+            res
+            .status(403)
+            .json({ success: false, message: "Usuário sem permissão para essa transação" }).end();
+        }
+        
+        req.usuario = usuario;
+        next()
+    });
+}
 
 routes.get("/usuario", UsuarioController.mostrarTipoUsuario); // mostrar tipo usuarios disponiveis
 routes.post("/usuario", UsuarioController.cadastrarUsuario); // cadastrar usuario
+routes.post("/usuario/login", UsuarioController.loginUsuario); // login usuario
+routes.post("/usuario/testarLogin", autenticarUsuario, UsuarioController.testarLogin); // testar autenticacao
 
 module.exports = routes;
